1e2017ef8643ba170a4c98b2308ebc4d47b8dc9d
Bart Riemens <briemens@crafity.com>
2013-08-12 16:09:21 +0200

    Implemented Jade functionality

--- main.js
+++ main.js
@@ -10,60 +10,87 @@
 /**
  * Module dependencies.
  */
-var jade = require('jade');
+var jade = require('jade')
+	, fs = require('crafity-filesystem');
 
 /**
  *
- * @param config
+ * @param {Object} [config]
  * @returns {Function}
  */
 function init(config) {
-  if (!config) {
-    throw new Error("Argument 'config' is required");
-  }
+	config = config || {};
+	config.viewPath = config.viewPath || "views/";
+	config.layout = config.layout !== false ? "layout" : null;
 
-  /**
-   *
-   * @param req The http request
-   * @param res The http response
-   * @param {Function} [next] The next handler in the http pipeline
-   */
-  function handler(req, res, next) {
-    if (!req) {
-      throw new Error("Argument 'req' is required");
-    }
-    if (!res) {
-      throw new Error("Argument 'res' is required");
-    }
-    if (!next) {
-      throw new Error("Argument 'next' is required");
-    }
-    if (next && !(next instanceof Function)) {
-      throw new Error("Argument 'next' must be a function");
-    }
+	/**
+	 *
+	 * @param req The http request
+	 * @param res The http response
+	 * @param {Function} [next] The next handler in the http pipeline
+	 */
+	function handler(req, res, next) {
+		if (!req) {
+			throw new Error("Argument 'req' is required");
+		}
+		if (!res) {
+			throw new Error("Argument 'res' is required");
+		}
+		if (!next) {
+			throw new Error("Argument 'next' is required");
+		}
+		if (next && !(next instanceof Function)) {
+			throw new Error("Argument 'next' must be a function");
+		}
 
-    /**
-     *
-     * @param view
-     * @param model
-     */
-    function render(view, model) {
-      if (!view) {
-        throw new Error("Argument 'view' is required");
-      }
-      if (!model) {
-        throw new Error("Argument 'model' is required");
-      }
-      var data = "", fileContent = ""; // = console.log("jade", jade);
-      return res.end(jade.compile(fileContent)(data));
-    }
+		/**
+		 *
+		 * @param view
+		 * @param model
+		 * @param callback
+		 */
+		function render(view, model, callback) {
+			if (!view) {
+				throw new Error("Argument 'view' is required");
+			}
+			if (!model) {
+				throw new Error("Argument 'model' is required");
+			}
+			callback = callback || function () {
+				return;
+			};
 
-    res.render = render;
+			var jadeFilename = fs.combine(config.viewPath, view + ".jade")
+				, layoutFilename = fs.combine(config.viewPath, "layout.jade");
 
-    return next();
-  }
+			fs.readFile(jadeFilename, function (err, buffer) {
+				if (err) { return callback(err); }
 
-  return handler;
+				model.body = jade.compile(buffer.toString(), { filename: jadeFilename })(model || {});
+
+				fs.readFile(layoutFilename, function (err, buffer) {
+					if (err) { return callback(err); }
+					
+					var fileContent = buffer.toString()
+						, html = jade.compile(fileContent, { filename: layoutFilename })(model || {})
+						;
+					
+					//console.log("fileContent", html);
+					
+					res.setHeader('Content-Type', 'text/html');
+					res.end(html);
+					
+					return callback(null, html);
+				});
+			});
+		}
+
+		res.render = render;
+
+		return next();
+	}
+
+	return handler;
 }
 
 module.exports = init;
